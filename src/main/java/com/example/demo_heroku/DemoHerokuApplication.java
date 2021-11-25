300660ae6406ea903ddffec1561e1a12211a6dbd
Chawis Tungchaiwanna <chawis@Chawiss-MacBook-Pro.local>
2021-11-25 20:01:43 +0700

    init project

---
+++ src/main/java/com/example/demo_heroku/DemoHerokuApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo_heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController()
+@SpringBootApplication
+public class DemoHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHerokuApplication.class, args);
+	}
+
+	@GetMapping()
+	public String getName(){
+		return "Recycle Project Start";
+	}
+
+}
